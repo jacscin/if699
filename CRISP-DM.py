70277136cbe712a4a01780f2bd16373b9f522a7a
João Antônio <jacs@cin.ufpe.br>
2020-12-01 18:17:02 -0300

    First commit

---
+++ CRISP-DM.py
@@ -0,0 +1,25 @@
+# Business Understanding
+# What is the average credit of defaulters vs non defaulters?
+# What is the percentage of male defaulters vs non defaulters?
+# What is the percentage of female defaulters vs non defaulters?
+# What is the scolarity of defaulters vs non defaulters?
+# What is the marital status of defaulters vs non defaulters?
+# What is the average age of defaulters vs non defaulters?
+
+# Data Understanding
+# How many samples and features are in the data?
+# How many features have incorrect data?
+
+# Prepare Data
+# Fixing incorrect data
+
+# Model Data
+# KNN
+# Decision Tree
+# Random Forest
+# MLP
+# Ensemble MLP
+# Ensemble Classifiers
+
+
+# Results
